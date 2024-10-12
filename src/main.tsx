a21c2125fc245873af036f277d9fb68256e707d9
BeethorKapor <beethorcs@gmail.com>
2024-10-12 21:53:16 +0700

    the secondary commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import "./index.css";
-import store from "./redux/store.ts";
 import { Provider } from "react-redux";
-
+import store from "./redux/store.js";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <React.StrictMode>
